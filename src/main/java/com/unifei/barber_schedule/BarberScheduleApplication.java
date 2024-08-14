ff7141e7520e9e7b9db5d81e4683fd2afeac1cfb
Guilhermehrt02 <ghorita.gh@gmail.com>
2024-08-14 09:34:54 -0300

    first commit

---
+++ src/main/java/com/unifei/barber_schedule/BarberScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.unifei.barber_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifei/barber_schedule/BarberScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifei.barber_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
